f43a4c24e74ece890eba15647783bfac7967f5bc
MindCET <mindcetdev@gmail.com>
2026-06-04 15:25:36 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,7 @@
 // Copy this file to config.js and fill in your actual keys
 const AIRTABLE_TOKEN = 'YOUR_AIRTABLE_TOKEN';
 const AIRTABLE_BASE  = 'YOUR_AIRTABLE_BASE_ID';
-const AIRTABLE_TABLE = 'Teachers';
+const AIRTABLE_TABLE = 'teachers';
 
 const BUBBLE_URL   = 'https://YOUR_APP.bubbleapps.io/version-test/api/1.1/obj/user';
 const BUBBLE_TOKEN = 'YOUR_BUBBLE_TOKEN';
